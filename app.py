6aa19eecda93467224b4bd3faf0f1b1d8e6fc2f1
Kumaisha-cyber <Kumaishakumaisha86@gmail.com>
2026-07-24 19:24:21 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Secure Login System with 2FA"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
